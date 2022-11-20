cb9fb1b874ac83cbca7aacab5b7fe200c1672805
1-EXON <exon.indiv@gmail.com>
2022-11-20 18:28:27 +0900

    mongodb

--- L1/nosql_injection/mongodb.js
+++ L1/nosql_injection/mongodb.js
@@ -3,3 +3,19 @@ const app = express()
 const mongoose = require('mongoose')
 const db = mongoose.connection
 
+mongoose.connect('mongodb://localhost:27017/', { userNewUrlParser: true, useUnifiedTopology: true })
+app.get('/query', (req, res) => {
+    db.collection('user').find({
+        'uid': req.query.uid,
+        'upw': req.query.upw
+    }).toArray((err, result) => {
+        if (err) throw err
+        res.send(result)
+    })
+})
+
+const server = app.listen(3000, () => {
+    console.log('app.listen')
+})
+
+// 아 걍 서버 실제로 돌리진 않을게요
